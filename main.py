5162040faeb62d01ca8f024535cc06c5a1815aa9
Muhammed Malik Elhıdır <malekco721@gmail.com>
2025-04-07 21:53:30 +0300

    amnsbfmansf

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("merhaba jenkins")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def say_hello():
+    return "Hello from Python function!"
+
+@app.route('/')
+def index():
+    message = say_hello()  # نادينا الدالة
+    return render_template('index.html', msg=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
